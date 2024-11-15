355730b833934c5e3b98d9736fa5423f359d567c
HelloKeerthana <168259439+HelloKeerthana@users.noreply.github.com>
2024-11-16 00:06:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Profilepic from "./Profilepic";
+import Links from "./Links";
+import Skills from "./Skills"
+
+function app(){
+  return(
+    <>
+      <Profilepic/>
+      <Skills/>
+      <Links/>
+
+    </>
+);
+
+}
+
+export default app;
